e59e6c428e6d9b7c671d0c23da66ba118bd50dc1
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-18 16:48:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arr, str) {
+  const signedCards = [];
+  for (let i = 0; i < arr.length; i++) {
+    signedCards.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+  }
+  return signedCards;
+}
+
+function countDown(num) {
+  for (let i = 0; num >= i; num--) {
+    console.log(num);
+  }
+}
